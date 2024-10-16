b6df4ff8adab0ce533d0c87b8f0d62d9391a5145
Winston Neo <winstonseanqui@gmail.com>
2024-10-16 18:12:33 +0800

    fix husky setup

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,10 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
-          className="dark:invert"
+                            className="dark:invert"
           src="https://nextjs.org/icons/next.svg"
           alt="Next.js logo"
-          width={180}
+                        width={180}
           height={38}
           priority
         />
